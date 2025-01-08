c90b84e06c046b1299a5ec48104a41d15de78ac5
VIKAS <97907750+vikas70607@users.noreply.github.com>
2025-01-09 01:43:11 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,6 @@
+<script>
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://cdn.jsdelivr.net/gh/randeepdhesi/Widget-Bot/style.css';
+    document.head.appendChild(link);
+</script>
